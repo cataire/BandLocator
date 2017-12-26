f042f93ed55c326d051dd156e5bf865a981cba69
cataire <mail.for.mu@gmail.com>
2017-12-26 11:35:19 -0700

    map

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,15 @@
 // DU Web Dev Bootcamp 2017/2018 - Week-8 Homework - Project 1
 $(document).ready(function() {
 
-  
+  //add div for map
+  //get bands in town artist info
+  //get events locations
+  //populate map with markers
+
+ 
+
+
+
     });
     
     
