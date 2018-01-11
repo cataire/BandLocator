c47e34b1e8e4329c8bf51e56332367bcc7c82ab7
Jeffrey <phelpsjeffrey@yahoo.com>
2018-01-11 11:35:29 -0700

    More initial artist additions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -388,7 +388,7 @@ $.ajax({
 
 var initialArtists = ["Metallica", "A7X", "U2", "Offspring", "Bruno Mars", "Boz Scaggs", "Katy Perry", 
                         "Wyclef Jean", "Kid Rock", "Collective Soul", "Bryan Adams", "Kenny Chesney", 
-                        "Eagles", "Maroon 5", "Foreigner", "Shania Twain", "Brad Paisley", "pink", 
+                        "Eagles", "Maroon 5", "Foreigner", "Shania Twain", "Beats Antique", "pink", 
                         "Steely Dan", "Bon Jovi", "Brit Floyd", ];
 var initialArtist = initialArtists[Math.floor(Math.random() * initialArtists.length)];
 
