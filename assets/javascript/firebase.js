dbb9ae560fd48df97381e80e8fa2e7025f338258
Kgertenbach <karen.gertenbach@gmail.com>
2018-01-27 13:44:42 -0700

    Update firebase.js

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,45 +1,45 @@
-// Initialize Firebase
-  var config = {
-//     removed to prevent costs
-  };
-  firebase.initializeApp(config);
+// // Initialize Firebase
+//   var config = {
+// //     removed to prevent costs
+//   };
+//   firebase.initializeApp(config);
     
-  var database = firebase.database();
-  let venueCity;
-  let venueCountry;
-  let venueName;
+//   var database = firebase.database();
+//   let venueCity;
+//   let venueCountry;
+//   let venueName;
 
-  $('#search-btn').on('click', function(){
+//   $('#search-btn').on('click', function(){
 
-    // Retrieve user inputs from form
-    var query = $('#query').val().trim();
+//     // Retrieve user inputs from form
+//     var query = $('#query').val().trim();
  
-    // Create an object for new train to be added
-    var newArtist = {
-      createdAt:  new Date().toString(),
-      userSearched: query,
-      nextEventCity:venueCity,
-      nextEventCountry:venueCountry,
-      nextEventInfo:venueName,
-    }
+//     // Create an object for new train to be added
+//     var newArtist = {
+//       createdAt:  new Date().toString(),
+//       userSearched: query,
+//       nextEventCity:venueCity,
+//       nextEventCountry:venueCountry,
+//       nextEventInfo:venueName,
+//     }
   
-    console.log(newArtist.createdAt);
-    console.log('here is my new artist',newArtist)
-    database.ref().push(newArtist);
+//     console.log(newArtist.createdAt);
+//     console.log('here is my new artist',newArtist)
+//     database.ref().push(newArtist);
   
-    $('#query').val('');
+//     $('#query').val('');
 
-    return false;
+//     return false;
   
-  });
+//   });
   
   
-  database.ref().on('child_added', function(childSnapshot, prevChildKey) {
+//   database.ref().on('child_added', function(childSnapshot, prevChildKey) {
   
-    var query = childSnapshot.val().query;
+//     var query = childSnapshot.val().query;
   
   
 
-  });
+//   });
   
   
